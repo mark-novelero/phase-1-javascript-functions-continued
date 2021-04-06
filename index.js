f60695af6aee574b5e1ab22747af1bd896b5f01b
mark novelero <mark.novelero@gmail.com>
2021-04-05 20:58:38 -0400

    commit tree 68673ecee511fa9f801894d97b467a3f5dd08ffa

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Your code here
+function saturdayFun(activity){
+    if(activity === undefined){
+        return 'This Saturday, I want to roller-skate!';
+    } else{
+        return 'This Saturday, I want to ' + activity + '!';
+    }
+}
+
+function mondayWork(activity){
+    if(activity === undefined){
+        return 'This Monday, I will go to the office.'
+    } else{
+        return 'This Monday, I will ' + activity + '.'; 
+    }    
+}   
+
+function wrapAdjective(symbol = '||'){
+    
+}
